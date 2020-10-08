f6ebb4fc4a1077d1e21defbf373cef1ce5083a03
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-10-07 22:19:26 -0500

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,25 @@
+class Show  < ActiveRecord::Base
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+    def self.most_popular_show
+        top_rating = Show.maximum(:rating)
+        Show.where("rating = ?", top_rating).first
+    end
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+    def self.least_popular_show
+        bot_rating = Show.minimum(:rating)
+        Show.where("rating = ?", bot_rating).first
+    end
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+    def self.popular_shows
+        Show.where("rating > ?", 5)
+    end
+    def self.shows_by_alphabetical_order
+        Show.order(name: :asc)
+    end
+end
